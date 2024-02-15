a8d298df8d65bc16bc6c5936eadf639e1bb98305
Timothy Arlet <tarlet@cnm.edu>
2024-02-14 17:10:51 -0700

    UI in progress.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/GameFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/GameFragment.java
@@ -2,21 +2,29 @@ package edu.cnm.deepdive.codebreaker.controller;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Lifecycle.State;
 import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.ViewModelProvider;
+import edu.cnm.deepdive.codebreaker.R;
 import edu.cnm.deepdive.codebreaker.adapter.GuessesAdapter;
+import edu.cnm.deepdive.codebreaker.adapter.SwatchesAdapter;
 import edu.cnm.deepdive.codebreaker.databinding.FragmentGameBinding;
 import edu.cnm.deepdive.codebreaker.viewmodel.CodebreakerViewModel;
 
-public class GameFragment extends Fragment {
+public class GameFragment extends Fragment implements MenuProvider {
 
   private FragmentGameBinding binding;
   private CodebreakerViewModel viewModel;
+  private GuessesAdapter adapter;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -31,6 +39,7 @@ public class GameFragment extends Fragment {
     binding = FragmentGameBinding.inflate(inflater, container, false);
     binding.submit.setOnClickListener(
         (v) -> viewModel.submitGuess(binding.guess.getText().toString().strip()));
+    binding.colorSelector.setAdapter(new SwatchesAdapter(requireContext()));
     // TODO: 2/7/2024 Initialize any view widgets, as necessary.
     return binding.getRoot();
   }
@@ -38,6 +47,7 @@ public class GameFragment extends Fragment {
   @Override
   public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+    requireActivity().addMenuProvider(this, getViewLifecycleOwner(), State.RESUMED);
     // Connect to view models.
     viewModel = new ViewModelProvider(requireActivity()).get(CodebreakerViewModel.class);
     getLifecycle().addObserver(viewModel);
@@ -45,17 +55,36 @@ public class GameFragment extends Fragment {
     viewModel
         .getGame()
         .observe(owner, (game) -> {
-          GuessesAdapter adapter = new GuessesAdapter(requireContext(), game.getGuesses());
+          adapter = new GuessesAdapter(requireContext(), game.getGuesses());
           binding.guesses.setAdapter(adapter);
         });
     viewModel
         .getGuess()
         .observe(owner, (guess) -> {
-          ((GuessesAdapter) binding.guesses.getAdapter()).notifyDataSetChanged(); // TODO: 2/13/2024 scroll to make last guess visible.
+          if (adapter != null) {
+            adapter.notifyDataSetChanged();
+            binding.guesses.setSelection(adapter.getCount() - 1);
+            // TODO: 2/13/2024 scroll to make last guess visible.
+          }
         });
     viewModel
         .getInProgress()
         .observe(owner, (inProgress) -> {/* TODO Enable/disable controls on change of game state. */});
   }
 
+  @Override
+  public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+    menuInflater.inflate(R.menu.game_options, menu);
+  }
+
+  @Override
+  public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+    boolean handled = true;
+    if (menuItem.getItemId() == R.id.new_game) {
+      viewModel.startGame();
+    } else {
+      handled = false;
+    }
+    return handled;
+  }
 }
--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/MainActivity.java
@@ -9,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import dagger.hilt.android.AndroidEntryPoint;
 import edu.cnm.deepdive.codebreaker.R;
@@ -29,10 +31,10 @@ public class MainActivity extends AppCompatActivity {
     getLifecycle().addObserver(loginViewModel);
     loginViewModel
         .getAccount()
-            .observe(this, this::handleAccount);
+        .observe(this, this::handleAccount);
     loginViewModel
         .getThrowable()
-            .observe(this, this::handleThrowable);
+        .observe(this, this::handleThrowable);
     setContentView(binding.getRoot());
   }
 
@@ -48,6 +50,10 @@ public class MainActivity extends AppCompatActivity {
     boolean handled = true;
     if (item.getItemId() == R.id.sign_out) {
       loginViewModel.signOut();
+    } else if (item.getItemId() == R.id.settings) {
+      // TODO: 2/14/2024 navigate to settings.
+      Intent intent = new Intent(this, SettingsActivity.class);
+      startActivity(intent);
     } else {
       handled = super.onOptionsItemSelected(item);
     }
