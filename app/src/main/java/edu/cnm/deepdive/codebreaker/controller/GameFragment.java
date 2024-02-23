3bfcfda6019d9635279f7df63c9ad2840428de65
Timothy Arlet <tarlet@cnm.edu>
2024-02-23 13:16:03 -0700

    Fixed life cycle problem w/ adapter.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/GameFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/GameFragment.java
@@ -68,6 +68,7 @@ public class GameFragment extends Fragment implements MenuProvider {
   @Override
   public void onDestroyView() {
     binding = null;
+    adapter = null;
     super.onDestroyView();
   }
 
