6342c4b974246bb9c1906f4f668545c2c5cd995d
Timothy Arlet <tarlet@cnm.edu>
2024-02-28 12:01:17 -0700

    Replaced game result entity with database view.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/service/CodebreakerDatabase.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/service/CodebreakerDatabase.java
@@ -7,27 +7,43 @@ import androidx.room.RoomDatabase;
 import androidx.room.TypeConverter;
 import androidx.room.TypeConverters;
 import androidx.sqlite.db.SupportSQLiteDatabase;
+import edu.cnm.deepdive.codebreaker.model.dao.GameDao;
 import edu.cnm.deepdive.codebreaker.model.dao.GameResultDao;
+import edu.cnm.deepdive.codebreaker.model.dao.GuessDao;
 import edu.cnm.deepdive.codebreaker.model.dao.UserDao;
+import edu.cnm.deepdive.codebreaker.model.entity.Game;
 import edu.cnm.deepdive.codebreaker.model.entity.GameResult;
+import edu.cnm.deepdive.codebreaker.model.entity.Guess;
 import edu.cnm.deepdive.codebreaker.model.entity.User;
 import edu.cnm.deepdive.codebreaker.service.CodebreakerDatabase.Converters;
 import java.time.Duration;
 import java.time.Instant;
+import java.util.Date;
 
-@Database(entities = {User.class, GameResult.class}, version = 1)
+@Database(entities = {User.class, Game.class, Guess.class},
+    views = GameResult.class,
+    version = 1
+)
 @TypeConverters({Converters.class})
 public abstract class CodebreakerDatabase extends RoomDatabase {
 
-  public static final String NAME = "codebreaker_results";
+  private static final String NAME = "codebreaker";
 
   CodebreakerDatabase() {
     // avoid generation of javadoc HTML.
   }
 
+  public static String getName() {
+    return NAME;
+  }
+
   public abstract GameResultDao getGameResultDao();
 
   public abstract UserDao getUserDao();
+
+  public abstract GameDao getGameDao();
+
+  public abstract GuessDao getGuessDao();
 
   public static class Converters {
 
@@ -55,6 +71,17 @@ public abstract class CodebreakerDatabase extends RoomDatabase {
       return (value != null) ? Duration.ofMillis(value) : null;
     }
 
+    @TypeConverter
+    @Nullable
+    public static Long toLong(@Nullable Date value) {
+      return (value !=null) ? value.getTime() : null;
+    }
+
+    @TypeConverter
+    @Nullable
+    public static Date toDate(@Nullable Long value) {
+      return (value != null) ? new Date(value) : null;
+    }
   }
 
   public static class Callback extends RoomDatabase.Callback {
