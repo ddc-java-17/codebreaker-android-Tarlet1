c6187a536abb908218f8e85242e1de2e4524db77
Timothy Arlet <tarlet@cnm.edu>
2024-03-18 13:47:19 -0600

    Rankings in progress (working on UI).

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/RanksFragment.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/RanksFragment.java
@@ -2,23 +2,52 @@ package edu.cnm.deepdive.codebreaker.controller;
 
 import android.os.Bundle;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.ViewModelProvider;
 import edu.cnm.deepdive.codebreaker.R;
 import edu.cnm.deepdive.codebreaker.databinding.FragmentRanksBinding;
+import edu.cnm.deepdive.codebreaker.viewmodel.CodebreakerViewModel;
+import edu.cnm.deepdive.codebreaker.viewmodel.PreferencesViewModel;
+import edu.cnm.deepdive.codebreaker.viewmodel.RankingsViewModel;
 
 
 public class RanksFragment extends Fragment {
 
   private FragmentRanksBinding binding;
+  private RankingsViewModel rankingsViewModel;
 
   @Override
   public View onCreateView(
      @NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
     binding = FragmentRanksBinding.inflate(inflater, container, false);
     return binding.getRoot();
+  }
+
+  @Override
+  public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+    super.onViewCreated(view, savedInstanceState);
+    ViewModelProvider provider = new ViewModelProvider(requireActivity());
+    LifecycleOwner owner = getViewLifecycleOwner();
+    rankingsViewModel = provider.get(RankingsViewModel.class);
+    rankingsViewModel.getRankings()
+        .observe(owner, (rankings) -> {
+          // TODO: 3/18/2024 Populate recycler view with rankings.
+        });
+    CodebreakerViewModel codebreakerViewModel = provider.get(CodebreakerViewModel.class);
+    codebreakerViewModel.getGame()
+        .observe(owner, (game) -> {
+          // TODO: 3/18/2024 If we have both code length and games threshold, invoke rankingsViewModel.fetch(codelength, gamethreshold).
+        });
+    PreferencesViewModel preferencesViewModel = provider.get(PreferencesViewModel.class);
+    preferencesViewModel.getPreferredGamesThreshold()
+        .observe(owner, (threshold) -> {
+          // TODO: 3/18/2024 // TODO: 3/18/2024 If we have both code length and games threshold, invoke rankingsViewModel.fetch(codelength, gamethreshold).
+        });
   }
 
   @Override
