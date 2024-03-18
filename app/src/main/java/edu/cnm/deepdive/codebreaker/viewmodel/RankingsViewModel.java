d108e0d9fc23a5138e566ffed80c859253353c1d
Timothy Arlet <tarlet@cnm.edu>
2024-03-18 14:00:23 -0600

    Rankings in progress (working on UI, round trip verified).

--- app/src/main/java/edu/cnm/deepdive/codebreaker/viewmodel/RankingsViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/viewmodel/RankingsViewModel.java
@@ -30,7 +30,8 @@ public class RankingsViewModel extends ViewModel implements DefaultLifecycleObse
     rankings = new MutableLiveData<>();
     throwable = new MutableLiveData<>();
     pending = new CompositeDisposable();
-    // TODO: 3/18/2024 send initial query to service via repository.
+    // FIXME: 3/18/2024 remove line below.
+    fetch(4,2);
   }
 
   public LiveData<List<Ranking>> getRankings() {
